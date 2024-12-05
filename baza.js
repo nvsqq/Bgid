c0b800e14f6dc84cd02e1f3b1069cc9437daa495
nvsqq <bravlboks122@gmail.com>
2024-12-05 21:13:56 +0300

    oop

--- baza.js
+++ baza.js
@@ -43,85 +43,43 @@ showSlide(currentSlide);
 
 
 // интерактифчек с картой ()
+// script.js
+class MapSwitcher {
+    constructor(mapIds, buttonIds) {
+        this.maps = mapIds.map(id => document.getElementById(id));
+        this.buttons = buttonIds.map(id => document.getElementById(id));
+        this.init();
+    }
 
-const map1 = document.getElementById('map1');
-        const map2 = document.getElementById('map2');
-            const map3 = document.getElementById('map3');
-                const map4 = document.getElementById('map4');
-                    const map5 = document.getElementById('map5');
-                        map5.style.display = 'block'; 
+    init() {
+        this.maps.forEach((map, index) => {
+            map.style.display = index === 0 ? 'block' : 'none'; // Показываем первую карту
+            this.buttons[index].addEventListener('click', () => this.showMap(index));
+        });
+    }
 
-                    document.getElementById('showMap1').addEventListener('click', function() {
-                        showMap1.style.background = "#000";
-                        showMap1.style.color = "#fff";
-                        showMap1.style.transition = "all 1s";
-                        showMap2.style.background = "#D9D9D9";
-                        showMap2.style.color = "#000";
-                        showMap3.style.background = "#D9D9D9";
-                        showMap3.style.color = "#000";
-                        showMap4.style.background = "#D9D9D9";
-                        showMap4.style.color = "#000";
+    showMap(index) {
+        this.maps.forEach((map, i) => {
+            map.style.display = i === index ? 'block' : 'none'; // Показываем только выбранную карту
+            this.updateButtonStyle(i, index);
+        });
+    }
 
-                        map1.style.display = 'block';
-                        map2.style.display = 'none';   
-                        map3.style.display = 'none'; 
-                        map4.style.display = 'none'; 
-                        map5.style.display = 'none'; 
-                    });
+    updateButtonStyle(currentIndex, selectedIndex) {
+        this.buttons.forEach((button, i) => {
+            if (i === selectedIndex) {
+                button.style.background = "#000";
+                button.style.color = "#fff";
+            } else {
+                button.style.background = "#D9D9D9";
+                button.style.color = "#000";
+            }
+            button.style.transition = "all 1s";
+        });
+    }
+}
 
-                    document.getElementById('showMap2').addEventListener('click', function() {
-                        showMap1.style.background = "#D9D9D9";
-                        showMap1.style.color = "#000";
-                        showMap2.style.background = "#000";
-                        showMap2.style.color = "#fff";
-                        showMap2.style.transition = "all 1s";
-                        showMap3.style.background = "#D9D9D9";
-                        showMap3.style.color = "#000";
-                        showMap4.style.background = "#D9D9D9";
-                        showMap4.style.color = "#000";
-                        
-
-                        map1.style.display = 'none';
-                        map2.style.display = 'block';   
-                        map3.style.display = 'none'; 
-                        map4.style.display = 'none'; 
-                        map5.style.display = 'none'; 
-                    });
-
-                    document.getElementById('showMap3').addEventListener('click', function() {
-                        showMap1.style.background = "#D9D9D9";
-                        showMap1.style.color = "#000";
-                        showMap2.style.background = "#D9D9D9";
-                        showMap2.style.color = "#000";
-                        showMap3.style.background = "#000";
-                        showMap3.style.color = "#fff";
-                        showMap3.style.transition = "all 1s";
-                        showMap4.style.background = "#D9D9D9";
-                        showMap4.style.color = "#000";
-                        
-                      
-                        map1.style.display = 'none';
-                        map2.style.display = 'none';   
-                        map3.style.display = 'block'; 
-                        map4.style.display = 'none'; 
-                        map5.style.display = 'none'; 
-                    });
-
-                    document.getElementById('showMap4').addEventListener('click', function() {
-                        showMap1.style.background = "#D9D9D9";
-                        showMap1.style.color = "#000";
-                        showMap2.style.background = "#D9D9D9";
-                        showMap2.style.color = "#000";
-                        showMap3.style.background = "#D9D9D9";
-                        showMap3.style.color = "#000";
-                        showMap4.style.background = "#000";
-                        showMap4.style.color = "#fff";
-                        showMap4.style.transition = "all 1s";
-                        
-                      
-                        map1.style.display = 'none';
-                        map2.style.display = 'none';   
-                        map3.style.display = 'none'; 
-                        map4.style.display = 'block'; 
-                        map5.style.display = 'none'; 
-                    });
+// Инициализация класса с идентификаторами карт и кнопок
+const mapIds = ['map1', 'map2', 'map3', 'map4', 'map5'];
+const buttonIds = ['showMap1', 'showMap2', 'showMap3', 'showMap4'];
+const mapSwitcher = new MapSwitcher(mapIds, buttonIds);
